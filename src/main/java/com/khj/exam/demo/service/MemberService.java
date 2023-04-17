d367337d29ef699d13f8b5ae30aac7060edda9c1
kimhyeji <gpwl5589@gmail.com>
2023-04-17 18:29:58 +0900

    회원가입 시, 이름+이메일 중복체크

--- src/main/java/com/khj/exam/demo/service/MemberService.java
+++ src/main/java/com/khj/exam/demo/service/MemberService.java
@@ -10,25 +10,38 @@ import com.khj.exam.demo.vo.Member;
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
-	
+
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+
+		// 로그인아이디 중복체크
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if ( oldMember != null ) {
+
+		if (oldMember != null) {
 			return -1;
 		}
-		
+
+		// 이름+이메일 중복체크
+		oldMember = getMemberByNameAndEmail(name, email);
+
+		if (oldMember != null) {
+			return -2;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-		
+
 		return memberRepository.getLastInsertId();
 	}
-	
+
 	private Member getMemberByLoginId(String loginId) {
 		return memberRepository.getMemberByLoginId(loginId);
+	}
+
+	private Member getMemberByNameAndEmail(String name, String email) {
+		return memberRepository.getMemberByNameAndEmail(name, email);
 	}
 
 	public Member getMemberById(int id) {
