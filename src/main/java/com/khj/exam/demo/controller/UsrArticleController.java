063233d7f807887c1e55c2836b2c9f1be9da21ee
kimhyeji <gpwl5589@gmail.com>
2023-04-18 17:50:13 +0900

    게시물 작성 시, 로그인 여부 체크, 작성자 정보 저장

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -12,6 +12,8 @@ import com.khj.exam.demo.utill.Ut;
 import com.khj.exam.demo.vo.Article;
 import com.khj.exam.demo.vo.ResultData;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UsrArticleController {
 	@Autowired
@@ -20,7 +22,20 @@ public class UsrArticleController {
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData<Article> doAdd(String title, String body) {
+	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		if ( isLogined == false ) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+		
+		
 		if ( Ut.empty(title) ) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
@@ -29,7 +44,7 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "body(을)를 입력해주세요.");
 		}
 		
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 		
 		int id = writeArticleRd.getData1();
 		
