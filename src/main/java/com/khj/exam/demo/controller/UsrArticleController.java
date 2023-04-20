7a8d2ccf1f97000238f2c9d02f914dfdf629bb19
kimhyeji <gpwl5589@gmail.com>
2023-04-20 18:34:40 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들은 앞에서 거름

--- src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrArticleController.java
@@ -27,12 +27,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
 		Rq rq = (Rq)req.getAttribute("rq");
-		
-		if ( rq.isLogined() == false ) {
-			return ResultData.from("F-A", "로그인 후 이용해주세요.");
-		}
-		
-		
+				
 		if ( Ut.empty(title) ) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
