74589bd366601edd51ad7e11513fb0571a1d2a60
kimhyeji <gpwl5589@gmail.com>
2023-04-27 15:38:10 +0900

    좋아요, 싫어요 취소 처리

--- src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/khj/exam/demo/repository/ArticleRepository.java
@@ -11,11 +11,13 @@ import com.khj.exam.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 	public Article getForPrintArticle(@Param("id") int id);
-	
-	public List<Article> getForPrintArticles(int boardId, int limitStart, int limitTake, String searchKeywordTypeCode, String searchKeyword);
-	
-	public void writeArticle(@Param("memberId") int memberId, @Param("boardId") int boardId, @Param("title") String title, @Param("body") String body);
-	
+
+	public List<Article> getForPrintArticles(int boardId, int limitStart, int limitTake, String searchKeywordTypeCode,
+			String searchKeyword);
+
+	public void writeArticle(@Param("memberId") int memberId, @Param("boardId") int boardId,
+			@Param("title") String title, @Param("body") String body);
+
 	public void deleteArticle(@Param("id") int id);
 
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
@@ -29,6 +31,10 @@ public interface ArticleRepository {
 	public int getArticleHitCount(int id);
 
 	public int increaseGoodReactionPoint(int relId);
-	
+
 	public int increaseBadReactionPoint(int relId);
+
+	public int decreaseGoodReactionPoint(int relId);
+
+	public int decreaseBadReactionPoint(int relId);
 }
