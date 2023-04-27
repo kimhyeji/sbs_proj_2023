2afa334ee62f8f741871cc0aa092af01f7602e6e
kimhyeji <gpwl5589@gmail.com>
2023-04-27 18:51:02 +0900

    댓글 삭제, 수정 버튼 노출

--- src/main/java/com/khj/exam/demo/service/ReplyService.java
+++ src/main/java/com/khj/exam/demo/service/ReplyService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.khj.exam.demo.repository.ReplyRepository;
 import com.khj.exam.demo.utill.Ut;
+import com.khj.exam.demo.vo.Article;
 import com.khj.exam.demo.vo.Reply;
 import com.khj.exam.demo.vo.ResultData;
 
@@ -25,7 +26,48 @@ public class ReplyService {
 	}
 
 	public List<Reply> getForPrintReplies(int actorId, String relTypeCode, int relId) {
-		return replyRepository.getForPrintReplies(actorId, relTypeCode, relId);
+		List<Reply> replies = replyRepository.getForPrintReplies(actorId, relTypeCode, relId);
+		
+		for ( Reply reply : replies ) {
+			updateForPrintData(actorId, reply);
+		}
+		return replies;
+	}
+	
+	private void updateForPrintData(int actorId, Reply reply) {
+		if ( reply == null ) {
+			return;
+		}
+		
+		ResultData actorCanDelteRd = actorCanDelete(actorId, reply);
+		reply.setExtra__actorCanDelete(actorCanDelteRd.isSuccess());
+		
+		ResultData actorCanMoidyRd = actorCanModify(actorId, reply);
+		reply.setExtra__actorCanModify(actorCanMoidyRd.isSuccess());
+	}
+	
+	public ResultData actorCanModify(int actorId, Reply reply) {
+		if ( reply == null ) {
+			return ResultData.from("F-1", "댓글이 존재하지 않습니다.");
+		}
+		
+		if ( reply.getMemberId() != actorId ) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+		
+		return ResultData.from("S-1", "댓글 수정이 가능합니다.");
+	}
+	
+	public ResultData actorCanDelete(int actorId, Reply reply) {
+		if ( reply == null ) {
+			return ResultData.from("F-1", "댓글이 존재하지 않습니다.");
+		}
+		
+		if ( reply.getMemberId() != actorId ) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
+		
+		return ResultData.from("S-1", "댓글 삭제가 가능합니다.");
 	}
 	
 }
