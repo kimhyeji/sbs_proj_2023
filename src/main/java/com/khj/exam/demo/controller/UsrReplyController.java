f426d2ea09e4f8d114e180a38def964aa50f3ef3
sik2 <devpang20@gmail.com>
2023-04-28 14:26:43 +0900

    댓글 삭제

--- src/main/java/com/khj/exam/demo/controller/UsrReplyController.java
+++ src/main/java/com/khj/exam/demo/controller/UsrReplyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.khj.exam.demo.service.ReplyService;
 import com.khj.exam.demo.utill.Ut;
+import com.khj.exam.demo.vo.Reply;
 import com.khj.exam.demo.vo.ResultData;
 import com.khj.exam.demo.vo.Rq;
 
@@ -47,8 +48,37 @@ public class UsrReplyController {
 		}
 		
 		return rq.jsReplace(writeReplyRd.getMsg(), replceUri);
+	}
+	
+	@RequestMapping("/usr/reply/doDelete")
+	@ResponseBody
+	String doDelete(int id, String replceUri) {
+		if ( Ut.empty(id) ) {
+			return rq.jsHistoryBack("id(을)를 입력해주세요.");
+		}
+		
+		Reply reply = replyService.getForPrintReply(rq.getLoginedMemberId(), id);
+		
+		if ( reply == null ) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글이 존재하지 않습니다.", id));
+		}
+		
+		if ( reply.isExtra__actorCanDelete() == false) {
+			return rq.jsHistoryBack(Ut.f("%d번 댓글을 삭제할 권한이 없습니다..", id));
+		}
+		
+		ResultData deleteReplyRd = replyService.deleteReply(id);
+				
+		if ( Ut.empty( replceUri )) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+		
+		return rq.jsReplace(deleteReplyRd.getMsg(), replceUri);
 		
 	}
-
 
 }
