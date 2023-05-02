aea94fc43098a1bc116d559f382839d06e1d113c
sik2 <devpang20@gmail.com>
2023-05-02 16:32:42 +0900

    로그인 후 접근 할 수 없는 페이지에서는 afterLoginUri없이 로그인 페이지로 이동

--- src/main/java/com/khj/exam/demo/vo/Rq.java
+++ src/main/java/com/khj/exam/demo/vo/Rq.java
@@ -1,6 +1,7 @@
 package com.khj.exam.demo.vo;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -27,10 +28,13 @@ public class Rq {
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
 	private HttpSession session;
+	private Map<String, String> paramMap; 
 	
 	public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
 		this.req = req;
 		this.resp = resp;
+		
+		paramMap = Ut.getParamMap(req);
 		
 		this.session = req.getSession();
 		
@@ -118,5 +122,23 @@ public class Rq {
 	public void printReplaceJs(String msg, String uri) {
 		resp.setContentType("text/html; charset=UTF-8");
 		print(Ut.jsReplace(msg, uri));
+	}
+
+	public String getLoginUri() {
+		return "../member/login?afterLoginUri=" + getAfterLoginUri();
+	}
+
+	private String getAfterLoginUri() {
+		String requestUri = req.getRequestURI(); 
+		
+		switch (requestUri) {
+		case "/usr/member/login":
+		case "/usr/member/join":
+		case "/usr/member/findLoginId":
+		case "/usr/member/findLoginPw":
+			return Ut.getUriEncoded(Ut.getStrAttr(paramMap, "afterLoginUri", ""));
+		}
+		
+		return getEncodedCurrentUri();
 	}
 }
